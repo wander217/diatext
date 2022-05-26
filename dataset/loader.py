73fbfa6123c19e1ca92d11fd256e1337951bd1f2
Trinh_Thinh <Trinhthinh2171999@gmail.com>
2022-05-26 23:08:46 +0700

    initial

--- dataset/loader.py
+++ dataset/loader.py
@@ -90,14 +90,14 @@ class DetCollate:
             threshMaps.append(element['threshMap'])
             threshMasks.append(element['threshMask'])
             if "polygon" in element:
-                # for i in range(len(element['polygon'])):
-                #     tmp = element['polygon'][i]
-                #     x_min, x_max = np.min(tmp[:, 0]), np.max(tmp[:, 0])
-                #     y_min, y_max = np.min(tmp[:, 1]), np.max(tmp[:, 1])
-                #     element['polygon'][i] = np.array([
-                #         [x_min, y_min], [x_max, y_min],
-                #         [x_max, y_max], [x_min, y_max]
-                #     ])
+                for i in range(len(element['polygon'])):
+                    tmp = element['polygon'][i]
+                    x_min, x_max = np.min(tmp[:, 0]), np.max(tmp[:, 0])
+                    y_min, y_max = np.min(tmp[:, 1]), np.max(tmp[:, 1])
+                    element['polygon'][i] = np.array([
+                        [x_min, y_min], [x_max, y_min],
+                        [x_max, y_max], [x_min, y_max]
+                    ])
                 polygons.append(element['polygon'])
             if "orgShape" in element:
                 orgShapes.append(element['orgShape'])

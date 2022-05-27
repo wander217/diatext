a6b6949ef186579b5b8f2712007433d2a085c27f
thinhtq <Trinhthinh2171999@gmail.com>
2022-05-27 15:03:48 +0700

    initial

--- dataset/prep/detaug.py
+++ dataset/prep/detaug.py
@@ -74,10 +74,10 @@ class DetAug:
         for tar in data['target']:
             if self._onlyResize:
                 newPolygon: List = [(point[0], point[1])
-                                    for point in tar['polygon']]
+                                    for point in tar['bbox']]
             else:
                 keyPoints: List = [Keypoint(point[0], point[1])
-                                   for point in tar['polygon']]
+                                   for point in tar['bbox']]
                 keyPoints = aug.augment_keypoints([
                     KeypointsOnImage(keyPoints, shape=shape)
                 ])[0].keypoints
